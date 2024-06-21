68c8535c064aeb1a67d72184a79987cadc4a1cea
Dalton Tewanger <daltontewanger@yahoo.com>
2024-06-20 20:15:51 -0700

    updated configuration with application factory

--- app.py
+++ app.py
@@ -1,11 +1,2 @@
-# Config                    
-from flask import Flask
-app = Flask(__name__)
-
-# Routes
-@app.route('/')
-def index():
-    return 'Hello, this is not hello world!'
-@app.route('/pets')
-def pets(): 
-    return 'These are our pets available for adoption!'
+from petfax import create_app
+app = create_app()
